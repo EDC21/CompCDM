44d49c43f32122954d81db24a4bdd71f9beac7c6
An Chen <vk20927@bristol.ac.uk>
2023-01-20 16:28:39 +0000

    Comment out the nonlinear shear model

--- AxisymD.for
+++ AxisymD.for
@@ -225,13 +225,13 @@ C			  stressNew(i,4)= C44*stateNew(i,4)
 		  
 			  sgn4 = stateNew(i,4)/(abs(stateNew(i,4)+safety))
 		   
-			  IF(abs(stateNew(i,4)).gt.stateOld(i,5))THEN
-				stateNew(i,5)=abs(stateNew(i,4))
-				stressNew(i,4)=sgn4*(A*(one -exp(-B*stateNew(i,5))))  
-			  ELSE
-				stateNew(i,5)=stateOld(i,5)
-				stressNew(i,4)=sgn4*(A*(one -exp(-B*stateNew(i,5))) 
-     *				-G12*(stateNew(i,5)-abs(stateNew(i,4))))	 
+			  ! IF(abs(stateNew(i,4)).gt.stateOld(i,5))THEN
+				! stateNew(i,5)=abs(stateNew(i,4))
+				! stressNew(i,4)=sgn4*(A*(one -exp(-B*stateNew(i,5))))  
+			  ! ELSE
+				! stateNew(i,5)=stateOld(i,5)
+				! stressNew(i,4)=sgn4*(A*(one -exp(-B*stateNew(i,5))) 
+     ! *				-G12*(stateNew(i,5)-abs(stateNew(i,4))))	 
 			  ENDIF
 		  ENDDO
 	  ENDIF
